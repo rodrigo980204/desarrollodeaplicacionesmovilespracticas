e9f472247c38c85650bd91b39a9fb46e439c7636
rodrigo980204 <56887066+rodrigo980204@users.noreply.github.com>
2020-04-22 12:27:09 -0500

    Add files via upload

--- geo/sw1.js
+++ geo/sw1.js
@@ -1,3 +1,13 @@
-self.addEventListener('fetch', event => {
-  event.responseWith(fetch(event.request));
-});
+function soporteGeolocalizacion(){
+	navigator.geolocation.getCurrentPosition(mostrarPosicion,error,{timeout:1000} );
+}
+
+function mostrarPosicion(posicion) {
+	var latitud = posicion.coords.latitude;
+	var longitud = posicion.coords.longitude;
+	document.getElementById("longitud").innerHTML = "Longitud: "+longitud;
+	document.getElementById("latitud").innerHTML = "Latitud: "+latitud;
+}
+function error(errores){
+	alert(error.code);
+}
